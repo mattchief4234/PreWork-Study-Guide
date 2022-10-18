1b3c7412f3f30c62ae3cbd41d5dd59ccb1d2b0f5
mattchief4234 <mattchief4234@gmail.com>
2022-10-18 16:47:10 -0600

    added javascript

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,17 @@
-var topics = "HTML, CSS, GIT, JavaScript";
-console.log(topics);
+var topics = ['HTML', 'CSS', 'GIT', 'JavaScript'];
+for(var x = 0; x < topics.length; x++) {
+    console.log(topics[x])
+}
+/*
+if(topic === 'HTML') {
+    console.log("let's study HTML!");
+} else if(topic === 'CSS') {
+    console.log("let's study CSS!");
+} else if (topic === 'GIT') {
+    console.log("let's study GIT!");
+} else if (topic === 'JavaScriptd') {
+    console.log("let's study JavaScript!");
+} else {
+    console.log('Please try again!');
+}
+*/
